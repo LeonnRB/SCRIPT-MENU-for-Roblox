e92cea3bf52fe50d2748dc0803513175a0a32966
LeonnRB <leonidgpochta333@gmail.com>
2025-05-17 12:47:30 +0400

    Update LeonCheat.lua

--- LeonCheat.lua
+++ LeonCheat.lua
@@ -1,8 +1,21 @@
+local colors = {
+	-- Цвет фона у Секций
+    SchemeColor = Color3.fromRGB(150, 72, 148),
+	-- Цвет фона в правой части UI
+	Background = Color3.fromRGB(15,15,15),
+	-- Цвет фона в левой части UI
+    Header = Color3.fromRGB(15,15,15),
+	-- Цвет текста
+    TextColor = Color3.fromRGB(255,255,255),
+	-- Цвет фона у кнопок
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local character = game.Players.LocalPlayer.Character
 
 -- Создать окно UI
-local Window = Library.CreateLib("Leon Roblox Cheat", "RJTheme3")
+local Window = Library.CreateLib("Leon Roblox Cheat", "colors")
 
 -- Секция
 local Tab = Window:NewTab("Главное")
